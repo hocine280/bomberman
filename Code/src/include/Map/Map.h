e44e78ee1827195963697a92ac3a7766b7969c00
PietroCookie <contact@pierre-chemin.fr>
2022-03-05 14:00:20 +0100

    Début du code de la classe Map

--- Code/src/include/Map/Map.h
+++ Code/src/include/Map/Map.h
@@ -6,22 +6,23 @@
 class Map
 {
 	private:
-		int m_nbColumn = 8;
-		int m_nbLine = 8;
+		int m_nbColumn;
+		int m_nbLine;
 		Tile m_target;
 		Bomberman m_player;
-		Tile m_mapTile[8][8];
+		Tile** m_mapTile;
 		std::vector <Ennemy> m_listEnnemy;
 		std::vector <Item> m_listItems;
 
 	public:
-		Map();
+		Map(int nbLine = 8, int nbColumn = 8);
 		~Map();
 
 		Tile getTarget() const;
 		int getNbColumn() const;
 		int getNbLine() const;
 		Bomberman getPlayer() const;
+		void loadMap();
 		void showMap() const;
 };
 
--- Code/src/src/Map/Map.cpp
+++ Code/src/src/Map/Map.cpp
@@ -1,6 +1,9 @@
 #include "../../include/Map/Map.h"
 
-Map::Map() {}
+Map::Map(int nbLine, int nbColumn): m_nbLine(nbLine), m_nbColumn(nbColumn)
+{
+	loadMap();
+}
 
 Map::~Map()
 {
@@ -25,6 +28,24 @@ Tile Map::getTarget() const
 Bomberman Map::getPlayer() const
 {
 	return m_player;
+}
+
+void Map::loadMap()
+{
+	m_mapTile = (Tile**) malloc(nbLine * sizeof(Tile*));
+	for(int i=0; i<m_nbLine; i++)
+	{
+		m_mapTile[i] = (Tile*) malloc(m_nbColumn * sizeof(Tile));
+	}
+
+	for(i=0; i<m_nbLine; i++)
+	{
+		for(int j=0; j<m_nbColumn; j++)
+		{
+			Tile t();
+			m_mapTile[i][j] = &t;
+		}
+	}
 }
 
 void Map::showMap() const
